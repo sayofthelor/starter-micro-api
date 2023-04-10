dca9bd941e9fc2209f661cd125fdefb214dd70d0
sayofthelor <35895538+sayofthelor@users.noreply.github.com>
2023-04-10 09:35:59 -0400

    the app

--- index.js
+++ index.js
@@ -1,6 +1,21 @@
-var http = require('http');
-http.createServer(function (req, res) {
-    console.log(`Just got a request at ${req.url}!`)
-    res.write('Yo!');
-    res.end();
-}).listen(process.env.PORT || 3000);
+import http from 'http';
+import { ChatGPTAPI } from 'chatgpt';
+
+const port = 80;
+const gpt = new ChatGPTAPI({
+    apiKey: process.env.OPENAIKEY
+});
+
+const server = http.createServer(async (req, res) => {
+    req.on('data', async (data) => {
+        const message = data.toString();
+        console.log(message);
+        const response = await gpt.sendMessage(message);
+        console.log(response);
+        res.statusCode = 200;
+        res.setHeader('Content-Type', 'application/json');
+        res.end(response.text);
+    });
+});
+
+server.listen(port, () => console.log(`Server running on port ${port}`));
